198b21982dae57af792c1fa037754f07e85b6dd4
Enoch Nartey <yawenochnartey@gmail.com>
2026-08-30 00:26:45 +0200

    update: creating s3 storage bucket on hetzner for postgres backup

--- infra/main.tf
+++ infra/main.tf
@@ -6,11 +6,32 @@ terraform {
       source  = "hetznercloud/hcloud"
       version = "~> 1.45"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
 provider "hcloud" {
   token = var.hcloud_token
+}
+
+# aws provider for a bucket in hetzner
+provider "aws" {
+  alias      = "hetzner"
+  region     = var.location
+  access_key = var.hetzner_s3_access_key
+  secret_key = var.hetzner_s3_secret_key
+
+  endpoints {
+    s3 = "https://${var.location}.your-objectstorage.com"
+  }
+
+  skip_credentials_validation = true
+  skip_region_validation      = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
 }
 
 # networking module
@@ -37,4 +58,14 @@ module "compute" {
   firewall_ids  = [module.security.firewall_id]
   primary_ip_id = module.networking.primary_ip_id
   github_token = var.github_token
+}
+
+# storage module
+module "storage" {
+  source      = "./storage"
+  bucket_name = "${var.project_name}-backups"
+
+  providers = {
+    aws = aws.hetzner
+  }
 }
--- infra/output.tf
+++ infra/output.tf
@@ -1,3 +1,7 @@
 output "server_ip" {
   value = module.compute.server_ipv4
 }
+
+output "backup_bucket" {
+  value = module.storage.bucket_name
+}
